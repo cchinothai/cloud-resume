4708be8307b65d95e63c732bc634ba863f548496
Cody <cchinothai@gmail.com>
2025-12-29 19:36:44 -0800

    add default region to os.environ

--- cloud-resume-backend/lambda/handler.py
+++ cloud-resume-backend/lambda/handler.py
@@ -12,7 +12,7 @@ edge considerations
 import boto3
 import json
 import os 
-os.environ['AWS_DEFAULT_REGION'] = 'us-east-1' # add default region to run pytest in github actions
+os.environ['AWS_DEFAULT_REGION'] = 'us-east-1' # set default region to run pytest in github actions
 
 # API 
 
